df3833c1f9bca6b948e0e4fed984d453fdfb6481
Emily Conaghan <emily@huntandco.io>
2022-04-30 09:19:10 +1000

    She Codes Python Lesson 1 - Code

---
+++ variables_and_user_input/user_input_playground.py
@@ -0,0 +1,5 @@
+#name = input("What is your name? ")
+#print = (name)
+
+number = input ("Enter a number")
+print(int(number)+5)
---
+++ variables_and_user_input/variables_playground.py
@@ -0,0 +1,53 @@
+weather = "rainy"
+name = "Emily"
+
+#Line 5 prints string
+#print(weather)
+#print("rainy")
+
+# Data Types - String is a text data
+# Integer - Numerical Data (whole number)
+# Float numerical (with decimals)
+height = 155
+weight = 55.8
+#print (type (name))
+#print (type (height))
+#print (type (weight))
+
+day = 'Saturday'
+print(type(day))
+message= f"Today is {day}"
+message2 = "Today is {day}"
+print(message2)
+print (message)
+
+#Intergers
+#Run distance in m
+run1_dist = 1400
+run2_dist = 1800
+
+#Addition
+total_dist = run1_dist + run2_dist
+#print (total_dist)
+
+#Floats
+#Run Distance In km
+run3_dist = 1.7
+run4_dist = 1.4
+
+#Addition2
+total_dist = run3_dist + run4_dist
+#print (total_dist)
+
+#Division and Multiplication
+print (run1_dist / 1000)
+print (run3_dist * 1000)
+
+#Division is always produce floats
+#Other calculations depend on the data
+#As long as there is one float, float
+
+#Different Scenarios
+print(name + run2_dist)
+print (name *run2_dist)
+
